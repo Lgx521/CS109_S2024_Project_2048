6369ff820ceccdcb682a5d39ba260b1dada9025b
GAN Shengzhe <148550006+Lgx521@users.noreply.github.com>
2024-05-23 17:12:27 +0800

    Released_Version_3.1 Restore data changed the information

--- src/Main/Frame/SigninFrame.java
+++ src/Main/Frame/SigninFrame.java
@@ -187,6 +187,7 @@ public class SigninFrame extends JFrame implements ActionListener, ItemListener 
                 new LoginFrame().setup();
             } else if (userOperation.isUserConsistent(loginUserName.getText()) <= -2) {
                 JOptionPane.showMessageDialog(null, "Data file is modified!\nClick 'OK' to restore the data.");
+                //恢复数据
                 userOperation.restoreData();
                 return;
             }
