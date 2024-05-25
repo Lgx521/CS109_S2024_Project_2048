13b04580f9fa52505a64e7f4baea10a172d7d03e
GAN Shengzhe <148550006+Lgx521@users.noreply.github.com>
2024-05-25 15:55:57 +0800

    Resealed_Version_1.0

--- src/Test/getMD5.java
+++ src/Test/getMD5.java
@@ -8,6 +8,7 @@ public class getMD5 {
     public static void main(String[] args) {
         LoginAndSignIn loginAndSignIn = new LoginAndSignIn();
 
-        System.out.println(loginAndSignIn.md5HashCode("src/Main/Data/UserData.txt"));
+//        System.out.println(loginAndSignIn.md5HashCode("src/Main/Data/UserData.txt"));
+        System.out.println(loginAndSignIn.md5HashCode("src/Main/Data/Records/statisticsData_2_time.txt"));
     }
 }
